1e7d1e4242e51cc0a4e7131cf484db08442f7ca7
Watson Hartsoe <watson.hartsoe@gmail.com>
2025-07-15 03:40:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
@@ -26,4 +26,3 @@ export default function RootLayout({
     </html>
   );
 }
-
